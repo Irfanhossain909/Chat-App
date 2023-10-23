0fd691026b959dbc1b4c1e324712d1695c13eb55
Irfan hossain <irfanhpssain@gmail.com>
2023-10-23 20:51:15 +0600

    Front done

---
+++ app/src/main/java/com/example/chatapp/VerificationActivity.kt
@@ -0,0 +1,24 @@
+package com.example.chatapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.chatapp.databinding.ActivityVerificationBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class VerificationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityVerificationBinding
+    var auth:FirebaseAuth?=null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityVerificationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+        if (auth!!.currentUser!=null){
+            startActivity(Intent(this@VerificationActivity,OTPActivity::class.java))
+            finish()
+        }
+
+    }
+}
